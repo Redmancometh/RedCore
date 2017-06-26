cdd5926e625339127b76e3c018817f5ca3816805
Redmancometh <Brendanstcurry@gmail.com>
2017-06-25 23:41:03 -0500

    Move MenuTemplate and Pane into RedCore's configuration folder.

---
+++ src/com/redmancometh/redcore/config/MenuTemplate.java
@@ -0,0 +1,34 @@
+package com.redmancometh.redcore.config;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MenuTemplate implements Iterable<Pane>
+{
+    private List<Pane> panes = new ArrayList();
+
+    public List<Pane> getPanes()
+    {
+        return panes;
+    }
+
+    public void setPanes(List<Pane> panes)
+    {
+        this.panes = panes;
+    }
+
+    @Override
+    public void forEach(Consumer<? super Pane> action)
+    {
+        panes.forEach(action);
+    }
+
+    @Override
+    public Iterator<Pane> iterator()
+    {
+        return panes.iterator();
+    }
+
+}
---
+++ src/com/redmancometh/redcore/config/Pane.java
@@ -0,0 +1,43 @@
+package com.redmancometh.redcore.config;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Pane implements Iterable<Integer>
+{
+    private short color;
+    private List<Integer> indexes;
+
+    public List<Integer> getIndexes()
+    {
+        return indexes;
+    }
+
+    public short getColor()
+    {
+        return color;
+    }
+
+    public void setIndexes(List<Integer> indexes)
+    {
+        this.indexes = indexes;
+    }
+
+    public void setColor(short color)
+    {
+        this.color = color;
+    }
+
+    @Override
+    public void forEach(Consumer<? super Integer> action)
+    {
+        indexes.forEach(action);
+    }
+
+    @Override
+    public Iterator<Integer> iterator()
+    {
+        return indexes.iterator();
+    }
+}
