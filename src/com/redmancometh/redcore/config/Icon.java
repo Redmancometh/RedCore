8be5779dc44d5ee8a22f7eb5ab246ce19ef31d67
Dylan <Quantum64Development@gmail.com>
2017-07-01 16:59:06 -0400

    Format

--- src/com/redmancometh/redcore/config/Icon.java
+++ src/com/redmancometh/redcore/config/Icon.java
@@ -1,6 +1,7 @@
 package com.redmancometh.redcore.config;
 
 import java.util.List;
+
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
@@ -8,44 +9,44 @@ import com.redmancometh.redcore.util.ItemUtil;
 
 public class Icon
 {
-    private Material material;
-    private Short dataValue = 0;
-    private String displayName;
-    private List<String> lore;
-    
-    public ItemStack getIcon()
-    {
-        return ItemUtil.buildItem(material, displayName, dataValue, lore);
-    }
+	private Material material;
+	private Short dataValue = 0;
+	private String displayName;
+	private List<String> lore;
 
-    public short getDataValue()
-    {
-        return (dataValue != null) ? dataValue : 0;
-    }
+	public ItemStack getIcon()
+	{
+		return ItemUtil.buildItem(material, displayName, dataValue, lore);
+	}
 
-    public void setDataValue(short dataValue)
-    {
-        this.dataValue = dataValue;
-    }
+	public short getDataValue()
+	{
+		return (dataValue != null) ? dataValue : 0;
+	}
 
-    public String getDisplayName()
-    {
-        return displayName;
-    }
+	public void setDataValue(short dataValue)
+	{
+		this.dataValue = dataValue;
+	}
 
-    public void setDisplayName(String displayName)
-    {
-        this.displayName = displayName;
-    }
+	public String getDisplayName()
+	{
+		return displayName;
+	}
 
-    public List<String> getLore()
-    {
-        return lore;
-    }
+	public void setDisplayName(String displayName)
+	{
+		this.displayName = displayName;
+	}
 
-    public void setLore(List<String> lore)
-    {
-        this.lore = lore;
-    }
+	public List<String> getLore()
+	{
+		return lore;
+	}
+
+	public void setLore(List<String> lore)
+	{
+		this.lore = lore;
+	}
 
 }
